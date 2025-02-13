a992821ba8b9878cf5ed50bc364c5f6bcd83d33e
wjgr2004 <wjgriddell@icloud.com>
2025-02-13 17:13:59 +0000

    Connecting to RabbitMQ from service

--- src/template/example-service/main.py
+++ src/template/example-service/main.py
@@ -2,11 +2,27 @@
 Example service.
 """
 
+import os
+import time
+
+import pika
 
 def main():
     """
     Example main.
     """
+
+    env_vars = os.environ
+
+    credentials = pika.PlainCredentials(env_vars["RABBITMQ_USERNAME"], env_vars["RABBITMQ_PASSWORD"])
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(env_vars["RABBITMQ_SERVICE_HOST"], credentials=credentials)
+    )
+    channel = connection.channel()
+    
+    time.sleep(120)
+
     print("Hello from example-service 1")
 
 
