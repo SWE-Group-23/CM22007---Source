2567fd066744f86f0b15fc99e52525456c4cd558
peterc-s <petercs@purelymail.com>
2025-02-18 15:46:04 +0000

    Example usage of ScyllaDB

--- src/template/example-service/main.py
+++ src/template/example-service/main.py
@@ -5,10 +5,41 @@ Example service.
 import os
 
 import pika
+import cassandra.cluster as cc
+import cassandra.auth as ca
+import uuid
 
 
-def message_callback(ch, method, properties, body):
-    print(f"[RECEIVED] {body}")
+class CallbackHandler:
+    """
+    Class that contains information needed
+    for the RabbitMQ callback and a callback
+    method.
+    """
+
+    def __init__(self, scylla_session):
+        self.scylla_session = scylla_session
+
+    def message_callback(self, ch, method, properties, body):
+        """
+        The method called when a RabbitMQ message is
+        received.
+
+        For example purposes, it just puts the message in a messages
+        table with a random UUID.
+        """
+
+        print(f"[RECEIVED] {body}")
+        message = {
+            "id": uuid.uuid4(),
+            "message": body.decode(),
+        }
+        query = self.scylla_session.prepare(
+            """
+            INSERT INTO messages (id, message) VALUES (?, ?);
+            """
+        )
+        self.scylla_session.execute(query, message.values())
 
 
 def main():
@@ -18,11 +49,58 @@ def main():
 
     env_vars = os.environ
 
+    # Set up database cluster connection
+    cluster = cc.Cluster(
+        contact_points=[
+            "example-db-client.scylla.svc",
+        ],
+        auth_provider=ca.PlainTextAuthProvider(
+            # NOTE: default creds, will be changed
+            # to service user creds in future
+            username='cassandra',
+            password='cassandra'
+        )
+    )
+    session = cluster.connect()
+
+    # Create a keyspace for the service
+    # NOTE: this will be done by the CRD
+    session.execute(
+        """
+        CREATE KEYSPACE IF NOT EXISTS example_service
+            WITH REPLICATION = {
+                'class': 'SimpleStrategy',
+                'replication_factor': '3'
+            }
+            AND DURABLE_WRITES = true;
+        """
+    )
+
+    # Use that keyspace
+    session.set_keyspace("example_service")
+
+    # Create table for stuff
+    session.execute(
+        """
+        CREATE TABLE IF NOT EXISTS messages (
+            id UUID PRIMARY KEY,
+            message text,
+        )
+        """
+    )
+
+    # Create a callback handler with the scylla session
+    callback_handler = CallbackHandler(
+        scylla_session=session
+    )
+
+    # Get RabbitMQ service user credentials
     credentials = pika.PlainCredentials(
             env_vars["RABBITMQ_USERNAME"],
             env_vars["RABBITMQ_PASSWORD"],
     )
 
+    # Connect to RabbitMQ with credentials
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(
             "rabbitmq",
@@ -30,11 +108,13 @@ def main():
         )
     )
 
+    # Create channel
     channel = connection.channel()
 
+    # Configure and start consuming
     channel.basic_consume(
         queue="example-services-queue",
-        on_message_callback=message_callback,
+        on_message_callback=callback_handler.message_callback,
         auto_ack=True,
     )
 
