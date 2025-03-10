3b0894f045e89b2937b07715b9b928ab0eefff5c
peterc-s <petercs@purelymail.com>
2025-03-10 16:56:05 +0000

    appease pylint

--- src/shared/models/template.py
+++ src/shared/models/template.py
@@ -1,13 +1,28 @@
-from cassandra.cqlengine.models import Model
-from cassandra.cqlengine import columns
+"""
+CQL models for pings and pongs tables.
+"""
+
 import uuid
 
+from cassandra.cqlengine.models import Model
+from cassandra.cqlengine import columns
 
-class Pings(Model):
+
+class Pings(Model):  # pylint: disable=too-few-public-methods
+    """
+    Pings table:
+        id - UUID - Primary Key
+        message - Text
+    """
     id = columns.UUID(primary_key=True, default=uuid.uuid4)
     message = columns.Text()
 
 
-class Pongs(Model):
+class Pongs(Model):  # pylint: disable=too-few-public-methods
+    """
+    Pongs table:
+        id - UUID - Primary Key
+        message - Text
+    """
     id = columns.UUID(primary_key=True, default=uuid.uuid4)
     message = columns.Text()
