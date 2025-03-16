dfba4a4b6171deca7448979f7e906e7f4ea09656
amyh-3362 <amyhersey04@gmail.com>
2025-03-16 15:17:46 +0000

    Implemented send-message service

--- src/chat/send-message/main.py
+++ src/chat/send-message/main.py
@@ -3,15 +3,85 @@ Add some service docs here.
 """
 
 import os
+import json
+import logging
 
 import shared
 from shared import rpcs
+from shared.models import chat as models
+
+class SendMessageRPCServer(rpcs.RPCServer):
+    """
+    Subclass of RPCServer ...
+    """
+
+    def _add_message(self, chat_id, sender_id, time_sent, message):
+        """
+        Lil description
+        """
+        try:
+            models.Messages.create(
+                chat_id = chat_id,
+                sender_id = sender_id,
+                sent_time = time_sent,
+                message = message
+            )
+            return rpcs.response(200, {"message": "Message sent"})
+        except Exception as e:
+            return rpcs.response(400, {"message": "Unable to send message"})
+
+    def process(self, body, *args, **kwargs):
+        """
+        Respond with "Pong!", unless message
+        isn't "Ping!".
+        """
+        logging.info("[RECEIVED] %s", body.decode())
+
+        try:
+            req = json.loads(body)
+        except json.JSONDecodeError:
+            return rpcs.response(400, {"reason": "Bad JSON."})
+        
+        try:
+            if req["version"] != "1.0.0":
+                return rpcs.response(400, {"reason": "Bad version."})
+
+            resp = rpcs.response(500, {"reason": "Internal Server Error"})
+
+            chat_id = req["data"]["chat_id"]
+            sender_id = req["data"]["sender_id"]
+            time_sent = req["data"]["time_sent"]
+            message= req["data"]["message"]
+
+            resp = self._add_message(chat_id, sender_id, time_sent, message)
+
+            return resp
+        
+        except KeyError:
+            return rpcs.response(400, {"reason": "Malformed request."})
+        
+    
 
 def main():
     """
     Add appropriate docs here.
     """
-    raise NotImplementedError
+    # Set up database session
+    _ = shared.setup_scylla(
+        keyspace=os.environ["SCYLLADB_KEYSPACE"],
+        user=os.environ["SCYLLADB_USERNAME"],
+        password=os.environ["SCYLLADB_PASSWORD"],
+    )
+
+    rpc_server = SendMessageRPCServer(
+        os.environ["RABBITMQ_USERNAME"],
+        os.environ["RABBITMQ_PASSWORD"],
+        "sendmessage-rpc",
+    )
+
+    logging.info("Consuming...")
+    rpc_server.channel.start_consuming()
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
