ec6786d08f35fed04131b12b8f69536cce563e56
peterc-s <petercs@purelymail.com>
2025-03-17 09:42:39 +0000

    More tests for login user-pw stage

--- tests/integration/accounts/test_login_rpc.py
+++ tests/integration/accounts/test_login_rpc.py
@@ -32,8 +32,7 @@ class LoginRPCTest(AutocleanTestCase):
         super().setUp()
 
         # suppress new default session warning
-        logging.getLogger(
-            "cassandra.cqlengine.connection").setLevel(logging.ERROR)
+        logging.getLogger("cassandra.cqlengine.connection").setLevel(logging.ERROR)
 
         # setup valkey
         self.vk = valkey.Valkey(
@@ -74,18 +73,22 @@ class LoginRPCTest(AutocleanTestCase):
         """
         Creates a user in the database.
         """
+        pw_digest = self.ph.hash(
+            password,
+            salt=username.ljust(16, "=").encode(),
+        )
+
         (
             model.Accounts.if_not_exists().create(
                 username=username,
-                password_hash=self.ph.hash(
-                    self.ph.hash(
-                        password, salt=username.ljust(16, "=").encode())
-                ),
+                password_hash=self.ph.hash(pw_digest),
                 otp_secret=pyotp.random_base32(),
                 backup_code_hash=self.ph.hash(backup_code),
                 created_at=datetime.datetime.now(),
             )
         )
+
+        return pw_digest
 
     def test_username_password(self):
         """
@@ -100,8 +103,12 @@ class LoginRPCTest(AutocleanTestCase):
         usernames = ["Exists", "NotExists"]
         passwords = ["dummy-password", "not-the-password"]
         combinations = [
-            (str(uuid.uuid4()), user, pw, user ==
-             "Exists" and pw == "dummy-password")
+            (
+                str(uuid.uuid4()),
+                user,
+                pw,
+                user == "Exists" and pw == "dummy-password",
+            )
             for user in usernames
             for pw in passwords
         ]
@@ -128,3 +135,95 @@ class LoginRPCTest(AutocleanTestCase):
                     self.assertEqual(vk_data["username"], user)
                 else:
                     self.assertIsNone(vk_data_raw)
+
+    def test_malformed_username_password(self):
+        """
+        Tests a variety of different malformed
+        username and password combinations.
+        """
+
+        # dummy user
+        pw_digest = self._create_user()
+
+        # combinations to test
+        usernames = [
+            "Exists",
+            True,
+            1,
+            1.0,
+            ["Exists"],
+            {"username": "Exists"},
+        ]
+        passwords = [
+            pw_digest,
+            True,
+            1,
+            1.0,
+            [pw_digest],
+            {"password_digest": pw_digest},
+        ]
+
+        for i, user in enumerate(usernames):
+            for j, pw in enumerate(passwords):
+                correct = False
+                if i == j == 0:
+                    correct = True
+
+                token = str(uuid.uuid4())
+
+                with self.subTest(
+                    token=token,
+                    user=user,
+                    pw=pw,
+                    correct=correct,
+                ):
+                    resp = self.login_client.user_pw_call(
+                        token,
+                        "testing",
+                        user,
+                        pw,
+                    )
+
+                    if correct:
+                        self.assertEqual(resp["status"], 200)
+                        self.assertTrue(resp["data"]["correct"])
+                    else:
+                        self.assertEqual(resp["status"], 400)
+                        self.assertEqual(
+                            resp["data"]["reason"],
+                            "Malformed request.",
+                        )
+
+                    # check Valkey
+                    vk_data_raw = self.vk.get(f"login:{token}")
+                    if correct:
+                        self.assertIsNotNone(vk_data_raw)
+                        vk_data = json.loads(vk_data_raw)
+                        self.assertEqual(vk_data["stage"], "username-password")
+                        self.assertEqual(vk_data["username"], user)
+                    else:
+                        self.assertIsNone(vk_data_raw)
+
+    def test_username_password_wrong_stage(self):
+        """
+        Tests the username-password call where
+        the token has the wrong Valkey stage.
+        """
+        pw_digest = self._create_user()
+        token = str(uuid.uuid4())
+        stage = json.dumps({"step": "username-password", "username": "Exists"})
+
+        self.vk.set(f"login:{token}", stage)
+
+        resp = self.login_client.user_pw_call(
+            token,
+            "testing",
+            "Exists",
+            pw_digest,
+        )
+
+        self.assertEqual(resp["status"], 403)
+        self.assertEqual(resp["data"]["reason"], "Token not at correct step.")
+
+        vk_raw = self.vk.get(f"login:{token}")
+        self.assertEqual(stage, vk_raw.decode())
