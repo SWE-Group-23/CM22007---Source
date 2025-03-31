87b7b253784f995cdc93aadcc1bf509893a94843
amyh-3362 <amyhersey04@gmail.com>
2025-03-31 18:22:14 +0100

    Created block user service

---
+++ src/chat/block-user/main.py
@@ -0,0 +1,76 @@
+"""
+Blocks a chat between users
+"""
+
+import os
+import json
+import logging
+import uuid
+
+import shared
+from shared import rpcs
+from shared.models import chat as models
+
+class BlockUserRPCServer(rpcs.RPCServer):
+    """
+    Subclass of RPCServer
+    """
+    def __init__(self, rabbitmq_user: str, rabbitmq_pass: str, *, rpc_prefix="block-user-rpc"):
+        super().__init__(rabbitmq_user, rabbitmq_pass, rpc_prefix)
+
+    def _block_chat(self, chat_id):
+        """
+        Attempts to add a message to the messages table
+        """
+        try:
+            models.Chats.objects(chat_id=chat_id).update(blocked=True)
+            return rpcs.response(200, {"message": "Success"})
+        except ConnectionError as e:
+            logging.error("[DB ERROR] %s", e, exc_info=True)
+            return rpcs.response(400, {"message": "Error blocking user"})
+
+    def process(self, body):
+        logging.info("[RECEIVED] %s", body.decode())
+
+        try:
+            req = json.loads(body)
+        except json.JSONDecodeError:
+            return rpcs.response(400, {"reason": "Bad JSON."})
+
+        try:
+            if req["version"] != "1.0.0":
+                return rpcs.response(400, {"reason": "Bad version."})
+
+            resp = rpcs.response(500, {"reason": "Internal Server Error"})
+
+            chat_id = uuid.UUID(req["data"]["chat_id"])
+
+            resp = self._block_chat(chat_id)
+
+            return resp
+
+        except KeyError:
+            return rpcs.response(400, {"reason": "Malformed request."})
+
+def main():
+    """
+    Sets up Scylla, and the RPC Server, then starts
+    consuming the call queue.
+    """
+    _ = shared.setup_scylla(
+        keyspace=os.environ["SCYLLADB_KEYSPACE"],
+        user=os.environ["SCYLLADB_USERNAME"],
+        password=os.environ["SCYLLADB_PASSWORD"],
+    )
+
+    rpc_server = BlockUserRPCServer(
+        os.environ["RABBITMQ_USERNAME"],
+        os.environ["RABBITMQ_PASSWORD"]
+    )
+
+    logging.info("Consuming...")
+    rpc_server.channel.start_consuming()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
