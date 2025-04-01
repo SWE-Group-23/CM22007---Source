e2bba3ed151f7c0692930c0c4d8907d644481afb
peterc-s <petercs@purelymail.com>
2025-04-01 19:53:44 +0100

    register working

--- src/public-gateway/public-gateway/main.py
+++ src/public-gateway/public-gateway/main.py
@@ -5,16 +5,16 @@ Add some service docs here.
 import os
 from secrets import token_hex
 
-from quart import Quart, request, Response
+from quart import Quart, request
 import valkey
-from pika.exceptions import ChannelWrongStateError
-import argon2
 
-import shared
-from shared.rpcs.register_rpc import RegisterRPCClient
+from blueprints import register
+
 
 app = Quart(__name__)
+app.register_blueprint(register.blueprint)
 
+GW_NAME = "public-gateway"
 
 r = valkey.Valkey(
     host="public-gateway-valkey",
@@ -25,19 +25,6 @@ r = valkey.Valkey(
 )
 
 
-
-def create_register_client():
-    global register_client
-
-    register_client = RegisterRPCClient(
-        os.environ["RABBITMQ_USERNAME"],
-        os.environ["RABBITMQ_PASSWORD"],
-    )
-
-
-create_register_client()
-
-
 @app.before_request
 async def before_request():
     token = request.cookies.get("session-token")
@@ -45,75 +32,36 @@ async def before_request():
         # There could technically be a collison here
         token = token_hex(64)
         request.set_token = True
-        r.set(token, "{}", ex=86400*7)
+        r.set(token, "{}", ex=86400 * 7)
     else:
         request.set_token = False
-    
+
     request.token = token
+
 
 @app.after_request
 async def after_request(response):
     if request.set_token:
         # Should set secure to True when deployed
-        response.set_cookie("session-token", request.token, samesite="lax", httponly=True)
+        response.set_cookie(
+            "session-token", request.token, samesite="lax", httponly=True
+        )
 
     return response
-        
+
 
 @app.route("/")
 def index():
     return "Hello, world"
-
-@app.post("/register/check-valid-username")
-async def check_username():
-    json = await request.get_json()
-
-    if json is None or "username" not in json:
-        return Response(status=400)
-    
-    try:
-        resp = register_client.check_valid_username_call(
-            sid=request.token,
-            srv_from="public-gateway",
-            username=json["username"],
-        )
-    except ChannelWrongStateError:
-        create_register_client()
-
-    if resp["status"] != 200:
-        return Response(status=400)
-    
-    return resp["data"]
-
-
-@app.post("/register/check-password")
-async def check_password():
-    json = await request.get_json()
-
-    if json is None or "password" not in json:
-        return Response(status=400)
-    
-    try:
-        resp = register_client.check_valid_username_call(
-            sid=request.token,
-            srv_from="public-gateway",
-            username=json["username"],
-        )
-    except ChannelWrongStateError:
-        create_register_client()
-
-    if resp["status"] != 200:
-        return Response(status=400)
-    
-    return resp["data"]
 
 
 def main():
     """
     Add appropriate docs here.
     """
-    
+
     app.run(port=8080, debug=True)
+
 
 if __name__ == "__main__":
     main()
