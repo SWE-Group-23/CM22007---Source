1debd3fba95abeaaa82184af721f0dd755be72dd
peterc-s <petercs@purelymail.com>
2025-04-02 02:11:48 +0100

    formatting

--- src/shared/rpcs/send_message_rpc.py
+++ src/shared/rpcs/send_message_rpc.py
@@ -5,24 +5,27 @@ chat subsystem.
 
 from shared import rpcs
 
+
 class SendMessageRPCClient(rpcs.RPCClient):
     """
     RPC Client for using the send message RPC from the
     chat subsystem.
     """
+
     def __init__(self, *args, rpc_prefix="send-message-rpc", **kwargs):
         super().__init__(*args, rpc_prefix, **kwargs)
 
-    def call(   # pylint: disable=too-many-arguments,too-many-positional-arguments
-            self,
-            auth_user: str,
-            service: str,
-            chat_id,
-            sender_user: str,
-            receiver_user: str,
-            time_sent: int,
-            message: str,
-            api_version="1.0.0"):
+    def call(  # pylint: disable=too-many-arguments,too-many-positional-arguments
+        self,
+        auth_user: str,
+        service: str,
+        chat_id,
+        sender_user: str,
+        receiver_user: str,
+        time_sent: int,
+        message: str,
+        api_version="1.0.0",
+    ):
         """
         Calls send message RPC
         """
@@ -30,13 +33,13 @@ class SendMessageRPCClient(rpcs.RPCClient):
             auth_user,
             api_version,
             service,
-            data = {
+            data={
                 "chat_id": str(chat_id),
                 "sender_user": sender_user,
                 "receiver_user": receiver_user,
                 "time_sent": time_sent,
-                "message": message
-            }
+                "message": message,
+            },
         )
 
         return self._call(body=req)
